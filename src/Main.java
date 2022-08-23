2e404433fb28be0b28f5446907397f9b45b5cefe
Mr.M.V.BALA <mvbalakrishnan95@gmail.com>
2022-08-23 09:15:40 +0530

    it is an exercise.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        NumberInWord.printNumberInWord(5);
+        NumberInWord.printNumberInWord(0);
+        NumberInWord.printNumberInWord(1);
+        NumberInWord.printNumberInWord(-4);
+        NumberInWord.printNumberInWord(6);
+
     }
 }
